7c813c78fb4b13d7d3b32d5a2bb6a85352944de8
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-27 19:26:40 +0530

    Add environment variables for PORT and MongoDB connection; implement database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+// impORT FILE BUT NOT WORKING WHEN IT implemented in the code so to run i will use the below code
+//import dotenv from 'dotenv';
+//dotenv.config();
+
 
 const app = express();
-const PORT = 8008;
+const PORT = process.env.PORT||8008;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     
