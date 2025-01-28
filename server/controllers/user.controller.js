77f7658c8aa57da1c03b8bf4c56011ad9b380f2f
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-28 20:07:02 +0530

    Refactor user registration and login controllers with improved error handling and password hashing

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,27 +1,64 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 export const register = async (req, res) => {
-    try{
-        const{namr,email,password} = req.body;
-        if (!name || !email || !password){
+    try {
+        const { namr, email, password } = req.body;
+        if (!name || !email || !password) {
             return res.status(400).json({
-                success:false,
-                msg: "All fields are required"});
-            
+                success: false,
+                msg: "All fields are required"
+            });
+
         }
-        const user = await User.findne({email});
-        if (user){
+        const user = await User.findne({ email });
+        if (user) {
             return res.status(400).json({
-            success:false,
-            msg: "Email already exists"});
-            }
-            const hashedPassword = await bcrypt.hash(password, 10);
-            await User.create({
-                name,
-                email,
-                password,
-     })
-    }catch(error){
+                success: false,
+                msg: "Email already exists"
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        })
+        return res.status(201).json({
+            success: true,
+            message: "User created successfully"
+        });
+    } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "failed to register user"
+        })
+    }
+}
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password"
+            });
+        } 
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "failed to login"
+        })
     }
 }
