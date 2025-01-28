0310f6260430cb2b5bf2c2bf5ff35f607931a2d7
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-28 22:14:30 +0530

    `Added PORT variable and commented out API section`

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 const PORT = process.env.PORT || 8008;
+
+//api
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
