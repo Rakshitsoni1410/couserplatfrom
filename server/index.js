edf25623a0fdde9798ef11454cdfbe6da721bfd8
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-28 16:16:34 +0530

    `Added dotenv configuration and database connection to server/index.js`

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-
 import dotenv from 'dotenv';
-dotenv.config({});
+import connectDB from './database/db.js';
+
+dotenv.config();
+
+// Call data connection here
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT||8008;
+const PORT = process.env.PORT || 8008;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    
-})
+});
