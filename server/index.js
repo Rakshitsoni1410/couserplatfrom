fcd62259cc2c870c9cf41351acf588c422a770f3
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-29 14:37:09 +0530

    Fix CORS middleware implementation and update .gitignore file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
 import connectDB from './database/db.js';
 import userRoute from './routes/user.route.js';
+import cors from 'cors'
+
 
 dotenv.config();
 
@@ -16,14 +17,15 @@ const PORT = process.env.PORT || 8008;
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use({
-    origin:"http://localhost:8008"
-})
+app.use(cors({
+    origin: "http://localhost:8008",
+    credentials:true,
+}))
 // API routes
 app.use("/api/v1/user", userRoute);
 
 "http://localhost:8008/api/v1/user/register"
- // Corrected the placement of the closing brace here
+// Corrected the placement of the closing brace here
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
