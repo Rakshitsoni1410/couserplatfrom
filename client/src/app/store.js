f1f8b195310e24f775b21bee319deae261d2321c
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-01-31 21:14:06 +0530

    Fix import path for appStore in main.jsx and ensure newline at end of store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -5,3 +5,4 @@ export const appStore = configureStore({
         auth: authReducer,
     }   
 });
+4
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import appStore from './store'; // Import the appStore
+import { Provider } from "react-redux";  
+import {appStore} from './app/store'; // Import the appStore
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
