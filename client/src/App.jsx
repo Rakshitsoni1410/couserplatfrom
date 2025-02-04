509a0b62fd3e6e225b4325518c215ae561d1df52
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-04 23:12:27 +0530

    feat: refactor App component to use RouterProvider for improved routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
+import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/ui/Navbar"; // Ensure this path is correct
 import Login from "./pages/Login"; // Ensure this path is correct
 import HeroSection from "./pages/student/HeroSection";
 import MainLayout from "./layout/MainLayout";
@@ -15,21 +15,20 @@ const appRouter = createBrowserRouter([
         element: (
           <>
             <HeroSection />
-            {/* course*/}
+            {/* course section */}
           </>
         ),
+      },
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
 ]);
+
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <HeroSection />
-      <Login />
-    </main>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
