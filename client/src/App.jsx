4b6734d4a28718659257753303a4e9fbe786eb95
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-04 21:59:22 +0530

    feat: add HeroSection component to enhance course discovery experience in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@
 import'./App.css'
 import Navbar from './components/ui/Navbar'; // Ensure this path is correct
 import Login from './pages/Login'; // Ensure this path is correct
+import HeroSection from './pages/student/HeroSection';
 
 function App() {
   return (
     <main> 
       <Navbar/>
+      <HeroSection />
       <Login/>
     </main>
   );
