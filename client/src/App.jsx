1e63c87a2a8cb79e56bd679730c9c534d9fd85c7
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-04 22:30:35 +0530

    feat: implement MainLayout component and configure routing for improved app structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/ui/Navbar"; // Ensure this path is correct
+import Login from "./pages/Login"; // Ensure this path is correct
+import HeroSection from "./pages/student/HeroSection";
+import MainLayout from "./layout/MainLayout";
 
-import'./App.css'
-import Navbar from './components/ui/Navbar'; // Ensure this path is correct
-import Login from './pages/Login'; // Ensure this path is correct
-import HeroSection from './pages/student/HeroSection';
-
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            {/* course*/}
+          </>
+        ),
+      },
+    ],
+  },
+]);
 function App() {
   return (
-    <main> 
-      <Navbar/>
+    <main>
+      <Navbar />
       <HeroSection />
-      <Login/>
+      <Login />
     </main>
   );
 }
