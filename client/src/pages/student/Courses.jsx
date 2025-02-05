55cd16c75a3fcaf461b548b2c1acf5da0ddd66a3
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-05 23:33:41 +0530

    feat: reduce loading skeletons and course components to improve performance

--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -11,10 +11,10 @@ const Courses = () => {
         <h2 className="font-bold text-3xl text-center mb-10">Our Courses</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {isLoading
-            ? Array.from({ length: 8 }).map((_, index) => (
+            ? Array.from({ length: 1 }).map((_, index) => (
                 <CourseSkeleton key={index} />
               ))
-            : Array.from({ length: 8 }).map((_, index) => (
+            : Array.from({ length: 1 }).map((_, index) => (
                 <Course key={index} />
               ))}
         </div>
