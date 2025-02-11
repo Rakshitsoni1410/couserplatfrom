faabbbacb0367febf3cd19506db0192ce21fa435
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-11 19:38:22 +0530

    feat: enhance Profile component layout with styling and structure

--- client/src/pages/student/Profile.jsx
+++ client/src/pages/student/Profile.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="max-w-4xl mx-auto px-4 my-24">
+      <h1 className="font-bold text-2xl text-center md:text-left"> Profile</h1>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
