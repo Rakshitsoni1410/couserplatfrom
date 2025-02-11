22a56adefdf8e8cd95ff3b8621ef94829647bc05
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-11 21:57:56 +0530

    feat: update Avatar component size and layout in Profile page

--- client/src/pages/student/Profile.jsx
+++ client/src/pages/student/Profile.jsx
@@ -6,8 +6,8 @@ const Profile = () => {
     <div className="max-w-4xl mx-auto px-4 my-24">
       <h1 className="font-bold text-2xl text-center md:text-left">Profile</h1>
       <div className="flex flex-col md:flex-row  items-center md:items-start gap-8 my-5">
-        <div>
-        <Avatar className="h-8 w-8">
+        <div className="flex flex-col items-center">
+        <Avatar className="h-24 w-24 md:h-32 md:w-32 mb-4">
           <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
