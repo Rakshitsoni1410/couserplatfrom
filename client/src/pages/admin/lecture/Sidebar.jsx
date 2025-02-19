727996f5c17820546ee3593c6f004411b1704218
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-20 00:36:52 +0530

    `Removed launch.json and settings.json files, updated Sidebar.jsx with new imports and components`

--- client/src/pages/admin/lecture/Sidebar.jsx
+++ client/src/pages/admin/lecture/Sidebar.jsx
@@ -1,11 +1,16 @@
+import { ChartNoAxesColumn, Square, SquareLibrary } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <div className="hidden  lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-r-gray-300 dark:border-r-gray-700 bg-[#f0f0f0] p-5 sticky tops-0 h-screen">
-        <div>
-            
-        </div>
+      <div className="space-y-4">
+        <Link>
+          <ChartNoAxesColumn size={22} />
+          <h1>Dasboard</h1>
+        </Link>
+        <SquareLibrary/>
+      </div>
     </div>
   );
 };
