36a0b38201d30d6e24539cebcb53b267a83957f2
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-19 23:43:33 +0530

    `Added course route and updated index.js to include it`

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './database/db.js';
 import userRoute from './routes/user.route.js';
 import cors from 'cors';
-
+import courseRoute from './routes/course.route.js';
 dotenv.config();
 
 console.log("Connecting to Database...");
@@ -21,9 +21,9 @@ app.use(cors({
     origin: "http://localhost:5173", // Removed extra "http://"
     credentials: true,
 }));
-
+//api 
 app.use("/api/v1/user", userRoute); // This will handle all /api/v1/user routes
-
+app.use("/api/v1/course", courseRoute);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
