b0e38869d1d99dbbbdf944146acafbc5753e0295
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-20 00:20:19 +0530

    `Added initial implementation of Dashboard component in admin page`

---
+++ client/src/pages/admin/lecture/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="hidden  lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-r-gray-300 dark:border-r-gray-700 bg-[#f0f0f0] p-5 sticky tops-0 h-screen">
+        <div>
+            
+        </div>
+    </div>
+  );
+};
+
+export default Sidebar;
