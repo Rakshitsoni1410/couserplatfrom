c48f57be608a136579a2d65d9c444156e88c8a8b
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-19 23:58:43 +0530

    add2files

---
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddCourse = () => {
+  return (
+    <div className="flex-1 mx-10">
+      <div className="mb-4">
+        <h1 className="font-bold text-xl">
+          Lets Add Course,add some basic course details for your new course
+        </h1>
+        <p className="text-sm"> 
+          Empower your growth with our dynamic, user-friendly course management
+          tools!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AddCourse;
