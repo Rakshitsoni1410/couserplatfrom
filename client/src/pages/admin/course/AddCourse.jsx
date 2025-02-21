eba955289a9e4aa913e08b16a4451842fd89dd6c
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-21 22:09:32 +0530

    add button and Addcourses

--- client/src/pages/admin/course/AddCourse.jsx
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -1,3 +1,15 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Title } from "@radix-ui/react-dialog";
 import React from "react";
 
 const AddCourse = () => {
@@ -7,10 +19,38 @@ const AddCourse = () => {
         <h1 className="font-bold text-xl">
           Lets Add Course,add some basic course details for your new course
         </h1>
-        <p className="text-sm"> 
+        <p className="text-sm">
           Empower your growth with our dynamic, user-friendly course management
           tools!
         </p>
+      </div>
+      <div className="space-y-4">
+        <div>
+          <Label>Title</Label>
+          <Input
+            type="text"
+            name="courseTitle"
+            placeholder=" Your course name"
+          />
+        </div>
+        <div>
+          <Label>Category</Label>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select a category" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Category</SelectLabel>
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
     </div>
   );
