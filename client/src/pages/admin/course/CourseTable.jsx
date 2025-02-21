291b1c3bacf5433b8f53d75cf2aef017a2291b06
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-21 20:43:49 +0530

    `Updated column headers in CourseTable.jsx: changed "Invoice" to "Price", added "Title" and "Action" columns, removed "Method" and "Amount" columns.`

--- client/src/pages/admin/course/CourseTable.jsx
+++ client/src/pages/admin/course/CourseTable.jsx
@@ -33,10 +33,10 @@ function InvoiceTable() {
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead className="w-[100px]">Price</TableHead>
             <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead className="text-right">Action</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
