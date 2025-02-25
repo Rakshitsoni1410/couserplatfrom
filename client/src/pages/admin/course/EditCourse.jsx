bdf4026a9c9f2b2f7229deace945f9b311c8f6ae
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-25 23:09:48 +0530

    `Update EditCourse component with minor styling and routing changes`

--- client/src/pages/admin/course/EditCourse.jsx
+++ client/src/pages/admin/course/EditCourse.jsx
@@ -1,18 +1,20 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const EditCourse = () => {
   return (
-    <div className='flex-1 '>
-        <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-3xl font-bold'>Add details information regarding course </h1>
-            <Link>
-            <Button variant='link' >Go to lectures page</Button>
-            </Link>
-        </div>
+    <div className="flex-1 ">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-3xl font-bold">
+          Add details information regarding course{" "}
+        </h1>
+        <Link to="/lecture">
+          <Button className="hover:text-blue-600" variant="link">Go to lectures page</Button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditCourse
+export default EditCourse;
