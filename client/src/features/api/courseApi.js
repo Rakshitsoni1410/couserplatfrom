6961e127dc35c2802916b03b7399ba64f6470efb
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-25 22:43:10 +0530

    `Added refetch tags and navigation to AddCourse page`

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -4,6 +4,7 @@ const COURSE_API = "http://localhost:8008/api/v1/course";
 
 export const courseApi = createApi({
     reducerPath: 'courseApi',
+    tagTypes:['Refetch_Creator_Course'],
     baseQuery: fetchBaseQuery({
         baseUrl: COURSE_API,
         credentials: "include",
@@ -14,14 +15,16 @@ export const courseApi = createApi({
                 url: "/",
                 method: "POST",
                 body: { courseTitle, category, coursePrice },
-            })
+            }),
+            invalidatesTags:['Refetch_Creator_Course']
         }),
         getCreatorCourse: builder.query({
             query: () => ({
                 url: "", // Example: If your API provides all courses at this route
                 method: "GET",
-            })
-        })
+            }),
+            providesTags:['Refetch_Creator_Course']
+        }),
         
     })  
 })
