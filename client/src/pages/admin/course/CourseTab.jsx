d8644e5a14759ab2b8cd9c134f0024c82913981a
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-25 23:15:07 +0530

    add

--- client/src/pages/admin/course/CourseTab.jsx
+++ client/src/pages/admin/course/CourseTab.jsx
@@ -1,8 +1,26 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const CourseTab = () => {
   return (
-    <div>CourseTab</div>
+    <Card>
+        <Card.Header>
+            <div>
+                <CardTitle>
+                    Basic Course Information
+                </CardTitle>
+                <CardDescription>
+                    Make changes to the course here.Click save when you're done.
+                </CardDescription>
+            </div>
+            <div>
+                <Button>
+                    
+                </Button>
+            </div>
+        </Card.Header>
+    </Card>
   )
 }
 
--- client/src/pages/admin/course/EditCourse.jsx
+++ client/src/pages/admin/course/EditCourse.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { Link } from "react-router-dom";
+import CourseTab from "./CourseTab";
 
 const EditCourse = () => {
   return (
@@ -10,9 +11,12 @@ const EditCourse = () => {
           Add details information regarding course{" "}
         </h1>
         <Link to="/lecture">
-          <Button className="hover:text-blue-600" variant="link">Go to lectures page</Button>
+          <Button className="hover:text-blue-600" variant="link">
+            Go to lectures page
+          </Button>
         </Link>
       </div>
+      <CourseTab />
     </div>
   );
 };
