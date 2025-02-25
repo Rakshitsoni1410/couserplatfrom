f48e14298082200384946587d958a87f636d2b45
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-02-25 23:06:13 +0530

    `Added navigation and title to EditCourse page`

--- client/src/pages/admin/course/EditCourse.jsx
+++ client/src/pages/admin/course/EditCourse.jsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const EditCourse = () => {
   return (
-    <div>EditCourse</div>
+    <div className='flex-1 '>
+        <div className='flex items-center justify-between mb-5'>
+            <h1 className='text-3xl font-bold'>Add details information regarding course </h1>
+            <Link>
+            <Button variant='link' >Go to lectures page</Button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
