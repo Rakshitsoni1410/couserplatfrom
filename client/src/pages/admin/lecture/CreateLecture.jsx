3a0193aaecca28b3886332dcfaa6ad353381a62e
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-03-01 20:47:27 +0530

    `Added useParams hook and courseId parameter to CreateLecture component, updated button text and navigation links.`

--- client/src/pages/admin/lecture/CreateLecture.jsx
+++ client/src/pages/admin/lecture/CreateLecture.jsx
@@ -3,18 +3,22 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
 import React, { useState } from "react"; // Import useState from React
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CreateLecture = () => {
   const [lectureTitle, setLectureTitle] = useState(""); // Using useState
+  const params = useParams();
+  const courseId = params.courseId;
   const isLoading = false;
   const navigate = useNavigate();
-
+  const createLectureHandler = async() => {
+    
+  }
   return (
     <div className="flex-1 mx-10">
       <div className="mb-4">
         <h1 className="font-bold text-xl">
-          Let's add a course, add some basic details for your new course
+          Let's add a course, add some basic details for your new lecture.
         </h1>
         <p className="text-sm">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
@@ -32,17 +36,17 @@ const CreateLecture = () => {
           />
         </div>
         <div className="flex items-center gap-2">
-          <Button variant="outline" onClick={() => navigate("/admin/course")}>
-            Back
+          <Button variant="outline" onClick={() => navigate(`/admin/course/${courseId}`)}>
+            Back to Course
           </Button>
-          <Button disabled={isLoading}>
+          <Button disabled={isLoading}onClick={createLectureHandler}>
             {isLoading ? (
               <>
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                 Please wait
               </>
             ) : (
-              "Create"
+              "Create Lecture"
             )}
           </Button>
         </div>
