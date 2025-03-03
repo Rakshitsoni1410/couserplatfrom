39531f3144aef93c3bb8aba9cbf25bc589d8ad09
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-03-03 23:20:18 +0530

    `Added @radix-ui/react-switch package and updated dependencies in package-lock.json and package.json. Updated EditLecture.jsx to use useParams and Link from react-router-dom.`

---
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -0,0 +1,49 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import React from 'react'
+
+const LectureTab = () => {
+    return (
+        <Card>
+            <CardHeader className='flex justify-between'>
+                <div>
+                    <CardTitle>
+                        Edit Lecture
+                    </CardTitle>
+                    <CardDescription>Make changes to the lecture details</CardDescription>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <Button variant="destructive">Remove lecture</Button>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <div>
+                    <Label>Title</Label>
+                    <Input
+                        type="text"
+                        placeholder="Ex: Introduction to python"
+                    />
+                </div>
+                <div className='my-5'>
+                    <Label>video <span className='text-red-500'> *</span></Label>
+                    <Input
+                        type="file"
+                        accept="video/*"
+                        placeholder="Ex: Introduction to python"
+                        className="w-fit"
+                    />
+                </div>
+                <div className='flex items-center space-x-2 my-5'>
+                    <Switch id="airplane-mode" />
+                    <Label htmlFor="airplane-mode"> IS this video free</Label>
+
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LectureTab
