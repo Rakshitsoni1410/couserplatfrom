bae8862a57edccf8346d2eb09688d3cc768d6216
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-03-04 16:54:07 +0530

    `Added axios library and implemented video upload functionality`

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -9,12 +9,43 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
+const MEDIA_API = "http://localhost:8008/api/v1/media";
 const LectureTab = () => {
     const [title, setTitle] = useState("");
     const [uploadVideInfo,setUploadVideoInfo] = useState(null);
-     
+    const [isFree, setIsFree] = useState(false);
+    const [mediaProgress, setMediaProgress] = useState(false);
+    const [uploadProgress, setUploadProgress] = useState(0);
+    const[btnDisabled, setBtnDisabled] = useState(true);
+
+    const fileChangeHandler = async(e) => {
+        const file = e.target.files[0];
+        if(file){
+          const formData = new FormData();
+          formData.append("file", file);
+          setMediaProgress(true);
+          try {
+            const res = await axios.post(`${MEDIA_API}/upload-video`,formData,{
+              onUploadProgress: ({loaded, total}) => {
+                  setUploadProgress(Math.round((loaded*100)/total));
+              }
+            })
+            if (res.data.success) {
+              console.log(res);
+              setUploadVideoInfo({videoUrl:res.data.data.url,publicId:res.data.data.public_id});
+              setBtnDisabled(false);
+              toast.success(res.data.message);              
+            }
+          } catch (error) {
+            console.log(error);
+            toast.error("Failed to upload video");
+          }
+        }
+    }
   return (
     <Card>
       <CardHeader className="flex justify-between">
--- server/routes/media.route.js
+++ server/routes/media.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import upload from '../utils/multer.js';
-import { uploadMedia } from '../utils/cloudinarys.js';
+import { uploadMedia } from '../utils/cloudinary.js';
+
 
 const router=express.Router();
 
@@ -8,6 +9,7 @@ router.route("/upload-video").post(upload.single("file"), async(req,res)=>{
     try {
         const result = await uploadMedia(req.file.path);
         return res.status(200).json({
+            success: true,
             message: "Video uploaded successfully.",
             data: result
         });
