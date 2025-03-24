46f9dbe005830414b00136817851c3ec2b6d9830
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-03-24 20:18:25 +0530

    `feat: updated CourseProgress.jsx with new UI components and layout`

--- client/src/pages/student/CourseProgress.jsx
+++ client/src/pages/student/CourseProgress.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { CheckCircle2, CirclePlay } from 'lucide-react';
+import React from 'react';
 
 const CourseProgress = () => {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-2xl font-bold text-blue-600">Course Progress</h1>
-      <p className="text-lg text-gray-700 mt-4">🎯 You are at the progress page!</p>
-    </div>
-  )
-}
+  const isCompleted = true;
 
-export default CourseProgress
+  return (
+    <div className='max-w-7xl mx-auto p-4 mt-20'>
+      {/* Course Title and Completed Button */}
+      <div className='flex justify-between mb-4'>
+        <h1 className='text-2xl font-bold'>Course Title</h1>
+        <Button>Completed</Button>
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-6'>
+        {/* Video Section */}
+        <div className='flex-1 md:w-3/5 h-fit rounded-lg shadow-lg p-4'>
+          {/* Video placeholder */}
+          {/* <video /> */}
+
+          {/* Current Lecture Title */}
+          <div className='mt-2'>
+            <h3 className='font-medium text-lg'>Lecture 1: Introduction to HTML</h3>
+          </div>
+        </div>
+
+        {/* Lecture Sidebar */}
+        <div className='flex flex-col w-full md:w-2/5 border-t md:border-t-0 md:border-l border-gray-200 md:pl-4 md:pt-4 max-h-[500px] overflow-y-auto'>
+          <h2 className='font-semibold text-xl mb-4'>Course Lectures</h2>
+
+          {
+            [1, 2, 3, 4].map((lecture, idx) => (
+              <Card key={idx} className='mb-3 hover:cursor-pointer transition-transform'>
+                <CardContent className='flex items-center justify-between p-4'>
+                  <div className='flex items-center'>
+                    {
+                      isCompleted ? (
+                        <CheckCircle2 size={24} className='text-green-500 mr-2' />
+                      ) : (
+                        <CirclePlay size={24} className='text-gray-500 mr-2' />
+                      )
+                    }
+                    <div>
+                      <CardTitle className="text-lg font-medium"> Introduction</CardTitle>
+                    </div>
+                  </div>
+                  <Badge variant={'outline'} className="bg-green-200 text-green-600">
+                    Completed
+                    </Badge>
+                </CardContent>
+              </Card>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseProgress;
