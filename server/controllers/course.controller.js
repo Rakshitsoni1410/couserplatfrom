060c356af5a23ef543e8c7d52527dade9f16e260
Rakshitson1410 <rakshitrsoni@gmail.com>
2025-03-26 19:42:18 +0530

    `Added searchCourse function and route to course controller and route respectively`

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -25,6 +25,47 @@ export const createCourse = async (req, res) => {
     res.status(500).json({ message: "Failed to create course" });
   }
 };
+//serach 
+export const searchCourse = async (req,res) => {
+  try {
+      const {query = "", categories = [], sortByPrice =""} = req.query;
+      console.log(categories);
+      
+      // create search query
+      const searchCriteria = {
+          isPublished:true,
+          $or:[
+              {courseTitle: {$regex:query, $options:"i"}},
+              {subTitle: {$regex:query, $options:"i"}},
+              {category: {$regex:query, $options:"i"}},
+          ]
+      }
+
+      // if categories selected
+      if(categories.length > 0) {
+          searchCriteria.category = {$in: categories};
+      }
+
+      // define sorting order
+      const sortOptions = {};
+      if(sortByPrice === "low"){
+          sortOptions.coursePrice = 1;//sort by price in ascending
+      }else if(sortByPrice === "high"){
+          sortOptions.coursePrice = -1; // descending
+      }
+
+      let courses = await Course.find(searchCriteria).populate({path:"creator", select:"name photoUrl"}).sort(sortOptions);
+
+      return res.status(200).json({
+          success:true,
+          courses: courses || []
+      });
+
+  } catch (error) {
+      console.log(error);
+      
+  }
+}
 //publish
 export const getPublishedCourse = async (req, res) => {
   try {
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
-import { createCourse,editCourse,getCourseById, getCreatorCourses,createCourseLecture, getCourseLecture, editLecture, removeLecture, getLectureById, togglePublishCourse, getPublishedCourse } from '../controllers/course.controller.js';
+import { createCourse,editCourse,getCourseById, getCreatorCourses,createCourseLecture, getCourseLecture, editLecture, removeLecture, getLectureById, togglePublishCourse, getPublishedCourse, searchCourse } from '../controllers/course.controller.js';
 import  upload from "../utils/multer.js";
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated,createCourse); 
+router.route("/search").get(isAuthenticated, searchCourse);
 router.route("/published-courses").get(isAuthenticated,getPublishedCourse);//publish course
 router.route("/").get(isAuthenticated,getCreatorCourses); 
 router.route("/:courseId").put(isAuthenticated, upload.single("courseThumbnail"), editCourse);
